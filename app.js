8d49fe7b7a1fa0ddd6c1305a0b5acfa4a53cdc54
hafizahteshamali <hafizahteshamali363617@gmail.com>
2024-09-03 19:41:12 +0500

    Responsive navbar update european suppliers

--- app.js
+++ app.js
@@ -39,4 +39,68 @@ const Modal = () => {
 menuIcon.addEventListener('click', Modal);
 closeIcon.addEventListener('click', Modal);
 
-AOS.init();
+
+let wholesaleLi =  document.querySelector('.wholesaleLi ul');
+let wholesaleList = document.getElementById('wholesaleList');
+
+const wholeSaleDropdown = (e)=>{
+    wholesaleLi.classList.toggle('wholesaleLiShow');
+    if(wholesaleLi.classList.contains('wholesaleLiShow')) {
+        wholesaleList.classList.add('rotate');
+    } else {
+        wholesaleList.classList.remove('rotate');
+    }
+}
+wholesaleList.addEventListener('click', wholeSaleDropdown);
+
+
+let aboutLi = document.querySelector('.aboutLi ul');
+let aboutList = document.getElementById('aboutList');
+const aboutDropDown = (e)=>{
+    aboutLi.classList.toggle('aboutDropDownShow'); 
+    if(aboutLi.classList.contains('aboutDropDownShow')) {
+        aboutList.classList.add('Aboutrotate');
+    } else {
+        aboutList.classList.remove('Aboutrotate');
+    }  
+}
+aboutList.addEventListener('click', aboutDropDown);
+
+
+
+let b2bli = document.querySelector('.b2bli ul');
+let b2bList = document.getElementById('b2bList');
+const b2bDropDown = (e)=>{
+    b2bli.classList.toggle('b2bDropDownShow');
+    if(b2bli.classList.contains('b2bDropDownShow')){
+        b2bList.classList.add('b2bRotate');
+    }
+    else{
+        b2bList.classList.remove('b2bRotate');
+    }
+}
+b2bList.addEventListener('click', b2bDropDown);
+
+
+let navlist = document.querySelectorAll('.navlist');
+let navicon = document.querySelectorAll('.navicon');
+
+const changingBackground = () => {
+    navlist.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            // Remove 'active' class from all navicons and navlists
+            navicon.forEach((content) => {
+                content.classList.remove('active');
+            });
+            navlist.forEach((tab) => {
+                tab.classList.remove('active');
+            });
+
+            // Add 'active' class to the clicked navlist and corresponding navicon
+            navicon[index].classList.add('active');
+            tab.classList.add('active');
+        });
+    });
+}
+
+changingBackground();
